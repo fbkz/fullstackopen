ba10b678e60ac14638f6aaedf50c4f294da6e154
Fábio Lopes <fabiolopes@tutanota.de>
2020-04-16 13:57:31 +0100

    1.2: course information, step2

--- part1/course_information/src/index.js
+++ part1/course_information/src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from "react-dom";
 
 const Header = ({ course }) => <h1>{course}</h1>;
 
+const Part = ({ part, exercises }) => (
+  <p>
+    {part} {exercises}
+  </p>
+);
+
 const Content = ({
   part1,
   part2,
@@ -11,17 +17,11 @@ const Content = ({
   exercises2,
   exercises3,
 }) => (
-  <>
-    <p>
-      {part1} {exercises1}
-    </p>
-    <p>
-      {part2} {exercises2}
-    </p>
-    <p>
-      {part3} {exercises3}
-    </p>
-  </>
+  <div>
+    <Part part={part1} exercises={exercises1} />
+    <Part part={part2} exercises={exercises2} />
+    <Part part={part3} exercises={exercises3} />
+  </div>
 );
 
 const Total = ({ value }) => <p>Number of exercises {value}</p>;
