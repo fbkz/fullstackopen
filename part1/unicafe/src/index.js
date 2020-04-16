162b93dbdae9ea69fabbc934bd324c7c8b532794
Fábio Lopes <fabiolopes@tutanota.de>
2020-04-16 16:21:25 +0100

    1.7: unicafe step2

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -5,18 +5,20 @@ const SubHeader = ({ text }) => <h2>{text}</h2>;
 const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>{text}</button>
 );
-const Part = ({ text, value }) => (
+const Part = ({ text, value, symbol }) => (
   <>
-    {text} {value}
+    {text} {value} {symbol}
+    <br />
   </>
 );
-const Content = ({ good, neutral, bad }) => (
+const Content = ({ good, neutral, bad, total, average, positivePerc }) => (
   <>
     <Part text="good" value={good} />
-    <br />
     <Part text="neutral" value={neutral} />
-    <br />
-    <Part text="bad" value={bad} />{" "}
+    <Part text="bad" value={bad} />
+    <Part text="all" value={total} />
+    <Part text="average" value={average} />
+    <Part text="positive" value={positivePerc} symbol="%" />
   </>
 );
 
@@ -25,6 +27,9 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const total = good + neutral + bad;
+  const average = (good - bad) / total || 0;
+  const positivePerc = (good / total) * 100 || 0;
 
   return (
     <div>
@@ -33,7 +38,14 @@ const App = () => {
       <Button text="neutral" handleClick={() => setNeutral(neutral + 1)} />
       <Button text="bad" handleClick={() => setBad(bad + 1)} />
       <SubHeader text="statistics" />
-      <Content good={good} neutral={neutral} bad={bad} />
+      <Content
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        total={total}
+        average={average}
+        positivePerc={positivePerc}
+      />
     </div>
   );
 };
