b9afa832dd5f31b999e089b28c4aee97c09bc6c7
Fábio Lopes <fabiolopes@tutanota.de>
2020-04-16 16:39:18 +0100

    1.8: unicafe step3

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -2,34 +2,40 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
 const SubHeader = ({ text }) => <h2>{text}</h2>;
+
 const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>{text}</button>
 );
+
 const Part = ({ text, value, symbol }) => (
   <>
     {text} {value} {symbol}
     <br />
   </>
 );
-const Content = ({ good, neutral, bad, total, average, positivePerc }) => (
-  <>
-    <Part text="good" value={good} />
-    <Part text="neutral" value={neutral} />
-    <Part text="bad" value={bad} />
-    <Part text="all" value={total} />
-    <Part text="average" value={average} />
-    <Part text="positive" value={positivePerc} symbol="%" />
-  </>
-);
+
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad;
+  const average = (good - bad) / total || 0;
+  const positivePerc = (good / total) * 100 || 0;
+
+  return (
+    <>
+      <Part text="good" value={good} />
+      <Part text="neutral" value={neutral} />
+      <Part text="bad" value={bad} />
+      <Part text="all" value={total} />
+      <Part text="average" value={average} />
+      <Part text="positive" value={positivePerc} symbol="%" />
+    </>
+  );
+};
 
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  const total = good + neutral + bad;
-  const average = (good - bad) / total || 0;
-  const positivePerc = (good / total) * 100 || 0;
 
   return (
     <div>
@@ -38,14 +44,7 @@ const App = () => {
       <Button text="neutral" handleClick={() => setNeutral(neutral + 1)} />
       <Button text="bad" handleClick={() => setBad(bad + 1)} />
       <SubHeader text="statistics" />
-      <Content
-        good={good}
-        neutral={neutral}
-        bad={bad}
-        total={total}
-        average={average}
-        positivePerc={positivePerc}
-      />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
