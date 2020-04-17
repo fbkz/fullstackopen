0fc6ebc79c8631ea00fa05481b63e48f13a866a2
Fábio Lopes <fabiolopes@tutanota.de>
2020-04-17 15:13:34 +0100

    2.11: The Phonebook Step6

--- part2/the_phonebook/src/App.js
+++ part2/the_phonebook/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Input = ({ text, value, handleChange }) => (
   <>
@@ -40,15 +41,16 @@ const Persons = ({ persons }) => {
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [newSearch, setSearch] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then(({ data }) => {
+      setPersons(data);
+    });
+  }, []);
 
   const handleNameChange = (e) => {
     setNewName(e.target.value);
