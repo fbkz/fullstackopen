ac8183c099eafd529820d5d25eb7bd2c15be3316
Fábio Lopes <fabiolopes@tutanota.de>
2020-04-17 12:53:56 +0100

    2.6: The Phonebook Step1

--- part2/the_phonebook/src/App.js
+++ part2/the_phonebook/src/App.js
@@ -4,19 +4,32 @@ const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
 
+  const handleNameChange = (e) => {
+    setNewName(e.target.value);
+  };
+
+  const addPerson = (e) => {
+    e.preventDefault();
+    setPersons(persons.concat({ name: newName }));
+    setNewName("");
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <ul style={{ listStyleType: "none" }}></ul>
+      {persons.map((person) => (
+        <li key={person.name}>{person.name}</li>
+      ))}
     </div>
   );
 };
