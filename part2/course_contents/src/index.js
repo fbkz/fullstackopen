3c444c5f80e5f4d7630e4e7498bd65a032921114
Fábio Lopes <fabiolopes@tutanota.de>
2020-04-17 12:02:27 +0100

    2.1: course contents step6

--- part2/course_contents/src/index.js
+++ part2/course_contents/src/index.js
@@ -12,7 +12,7 @@ const Part = ({ part, exercises }) => (
 const Content = ({ parts }) => (
   <div>
     {parts.map((part) => {
-      return <Part part={part.name} exercises={part.exercises} />;
+      return <Part key={part.id} part={part.name} exercises={part.exercises} />;
     })}
   </div>
 );
@@ -26,30 +26,52 @@ const Total = ({ parts }) => (
   </p>
 );
 
+const Course = ({ course }) => {
+  return (
+    <>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </>
+  );
+};
+
 const App = () => {
   const course = {
+    id: 1,
     name: "Half Stack application development",
     parts: [
       {
         name: "Fundamentals of React",
         exercises: 10,
+        id: 1,
       },
       {
         name: "Using props to pass data",
         exercises: 7,
+        id: 2,
       },
       {
         name: "State of a component",
         exercises: 14,
+        id: 3,
+      },
+      {
+        name: "State of a component",
+        exercises: 20,
+        id: 4,
+      },
+      {
+        name: "State of a component",
+        exercises: 20,
+        id: 5,
       },
     ],
   };
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <Course course={course} />
     </div>
   );
 };
