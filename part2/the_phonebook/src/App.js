e654c409e7082ffc9ba6cdb0105914d0d321b878
Fábio Lopes <fabiolopes@tutanota.de>
2020-04-17 14:46:16 +0100

    2.9*: The Phonebook Step4

--- part2/the_phonebook/src/App.js
+++ part2/the_phonebook/src/App.js
@@ -1,11 +1,35 @@
 import React, { useState } from "react";
 
+const Input = ({ text, value, handleChange }) => (
+  <>
+    {text} <input value={value} onChange={handleChange} />
+  </>
+);
+
+const SubHeader = ({ text }) => <h2>{text}</h2>;
+
+const Number = ({ name, number }) => <li>{`${name} ${number}`}</li>;
+
+const Numbers = ({ persons }) => {
+  return (
+    <ul style={{ listStyleType: "none", margin: 0, padding: 0 }}>
+      {persons.map((person) => (
+        <Number key={person.name} name={person.name} number={person.number} />
+      ))}
+    </ul>
+  );
+};
+
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-1234567" },
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [newSearch, setSearch] = useState("");
 
   const handleNameChange = (e) => {
     setNewName(e.target.value);
@@ -13,6 +37,10 @@ const App = () => {
 
   const handleNumberChange = (e) => {
     setNewNumber(e.target.value);
+  };
+
+  const handleSearchChange = (e) => {
+    setSearch(e.target.value);
   };
 
   const addPerson = (e) => {
@@ -31,24 +59,41 @@ const App = () => {
     alert(`${newName} is already added to phonebook`);
   };
 
+  const searchFilter = () => {
+    const searchPerson = (person) =>
+      person.name.toLowerCase().includes(newSearch.toLocaleLowerCase());
+    return persons.filter(searchPerson);
+  };
+
   return (
     <div>
-      <h2>Phonebook</h2>
+      <SubHeader text="Phonebook" />
+      <Input
+        text="filter shown with"
+        value={newSearch}
+        handleChange={handleSearchChange}
+      />
+      <SubHeader text="add a new" />
       <form onSubmit={addPerson}>
         <div>
-          name: <input value={newName} onChange={handleNameChange} />
+          <Input text="name:" value={newName} handleChange={handleNameChange} />
           <br />
-          number: <input value={newNumber} onChange={handleNumberChange} />
+          <Input
+            text="number:"
+            value={newNumber}
+            handleChange={handleNumberChange}
+          />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
-      <ul style={{ listStyleType: "none" }}></ul>
-      {persons.map((person) => (
-        <li key={person.name}>{`${person.name} ${person.number}`}</li>
-      ))}
+      <SubHeader text="Numbers" />
+      {newSearch ? (
+        <Numbers persons={searchFilter()} />
+      ) : (
+        <Numbers persons={persons} />
+      )}
     </div>
   );
 };
