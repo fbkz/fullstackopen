570ce1a10e625bbbacf58c5457040cb9859a9957
Fábio Lopes <fabiolopes@tutanota.de>
2020-04-20 08:35:41 +0100

    2.12* Data for countries, step1 [refactored]

--- part2/data_for_countries/src/App.js
+++ part2/data_for_countries/src/App.js
@@ -20,90 +20,64 @@ const Country = ({ name, capital, population, languages, flag }) => (
   </div>
 );
 
-const Countries = ({ countries }) =>
+const Countries = ({ countries, handleClick }) =>
   countries.map((country) => (
     <li key={country.alpha3Code}> {country.name} </li>
   ));
 
 const App = () => {
-  const [search, setSearch] = useState("");
   const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("");
+  const [search, setSearch] = useState("");
   const [searchResults, setSearchResults] = useState("");
-
-  const handleSearchChange = (e) => {
-    setSearch(e.target.value);
-  };
 
   useEffect(() => {
     axios.get("https://restcountries.eu/rest/v2/all").then(({ data }) => {
       setCountries(data);
     });
+  }, []);
 
-    const filterSearchResults = () => {
-      const filterCountries = (country) => {
-        const countryExists = country.name
-          .toLowerCase()
-          .includes(search.toLowerCase());
-        if (countryExists) {
-          return country;
-        }
+  const handleSearchChange = (e) => {
+    setCountry("");
+    setSearchResults("");
+    setSearch(e.target.value);
+  };
+
+  useEffect(() => {
+    const filterSearch = () => {
+      const filterSearchCB = (country) => {
+        return country.name.toLowerCase().includes(search.toLowerCase());
       };
+      const results = countries.filter(filterSearchCB);
 
-      let result = countries.filter(filterCountries);
-      if (result.length === 0) {
-        return "No country was found";
-      } else if (result.length <= 10) {
-        return result;
-      } else if (result.length > 10) {
-        return "Too many matches, specify another filter";
+      if (results.length === 1) {
+        setCountry(results);
+      } else if (results.length > 1 && results.length < 11) {
+        setSearchResults(results);
       }
     };
 
-    const filteredResults = filterSearchResults();
-    setSearchResults(filteredResults);
-  }, [search, countries]);
-
-  const renderText = () => {
-    if (typeof searchResults === "string") {
-      return <p>{searchResults}</p>;
-    }
-    return null;
-  };
-
-  const renderCountries = () => {
-    const arrayMultiCountries =
-      searchResults.length > 1 && Array.isArray(searchResults);
-    if (arrayMultiCountries) {
-      return <Countries countries={searchResults} />;
-    }
-    return null;
-  };
-
-  const renderCountry = () => {
-    const arraySingleCountry =
-      searchResults.length === 1 && Array.isArray(searchResults);
-    if (arraySingleCountry) {
-      return (
-        <Country
-          name={searchResults[0].name}
-          capital={searchResults[0].capital}
-          population={searchResults[0].population}
-          languages={searchResults[0].languages}
-          flag={searchResults[0].flag}
-        />
-      );
-    }
-    return null;
-  };
+    filterSearch();
+  }, [countries, search]);
 
   return (
     <div>
-      {" "}
       find countries <Input value={search} handleChange={handleSearchChange} />
       <br />
-      {search && renderText()}
-      {search && renderCountries()}
-      {search && renderCountry()}
+      {search && !country && !searchResults ? (
+        <p>Too many matches, specify another filter</p>
+      ) : null}
+      {country ? (
+        <Country
+          name={country[0].name}
+          capital={country[0].capital}
+          population={country[0].population}
+          languages={country[0].languages}
+          flag={country[0].flag}
+        />
+      ) : searchResults ? (
+        <Countries countries={searchResults} />
+      ) : null}
     </div>
   );
 };
