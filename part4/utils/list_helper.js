1fa9eb67da3a7043fdac48853b2c92bd3d63d135
Fábio Lopes <fabiolopes@tutanota.de>
2020-04-23 15:11:41 +0100

    4.4: helper functions and unit tests, step2

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((total, blog) => {
+    return blog.likes + total;
+  }, 0);
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
