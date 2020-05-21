370ae18019f18328e4331ec060bd64299fee3325
Fábio Lopes <fabiolopes@tutanota.de>
2020-05-21 17:52:29 +0100

    5.2: bloglist frontend, step2

--- part5/src/App.js
+++ part5/src/App.js
@@ -17,6 +17,7 @@ const App = () => {
         password,
       });
 
+      window.localStorage.setItem("loggedUser", JSON.stringify(user));
       setUser(user);
       setUsername("");
       setPassword("");
@@ -53,6 +54,14 @@ const App = () => {
   );
 
   useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem("loggedUser");
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+      setUser(user);
+    }
+  }, []);
+
+  useEffect(() => {
     blogService.getAll().then((blogs) => setBlogs(blogs));
   }, []);
 
@@ -62,7 +71,17 @@ const App = () => {
       {user != null && (
         <div>
           <h2>blogs</h2>
-          <p>{user.name} logged in</p>
+          <p>
+            {user.name} logged in{" "}
+            <button
+              onClick={() => {
+                window.localStorage.removeItem("loggedUser");
+                setUser(null);
+              }}
+            >
+              logout
+            </button>
+          </p>
         </div>
       )}
       {user != null && blogs.map((blog) => <Blog key={blog.id} blog={blog} />)}
