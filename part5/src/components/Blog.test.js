668f019befebbda49fb07e663464fb9960a8a8c7
Fábio Lopes <fabiolopes@tutanota.de>
2020-06-18 13:32:36 +0100

    5.14: Blog list tests, step2

--- part5/src/components/Blog.test.js
+++ part5/src/components/Blog.test.js
@@ -1,12 +1,36 @@
 import React from "react";
 import "@testing-library/jest-dom/extend-expect";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import Blog from "./Blog";
 
 test("renders title and author", () => {
-  let data = { likes: 90, title: "Caps blog", url: "caps.com", author: "Jin" };
+  let data = {
+    likes: 90,
+    title: "Caps blog",
+    url: "caps.com",
+    author: "Jin",
+  };
   let component = render(<Blog blog={data} />);
 
   expect(component.container).toHaveTextContent("Caps blog");
   expect(component.container).toHaveTextContent("Jin");
+});
+
+test("when clicking view button, blog's url and likes are shown", () => {
+  let data = {
+    likes: 90,
+    title: "Caps blog",
+    url: "caps.com",
+    author: "Jin",
+    user: {
+      name: "Rasmus",
+      user: "caps",
+    },
+  };
+  let component = render(<Blog blog={data} loggedUser />);
+
+  let button = component.getByText("view");
+  fireEvent.click(button);
+  expect(component.container).toHaveTextContent("caps.com");
+  expect(component.container).toHaveTextContent("90");
 });
